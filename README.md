1dbacaaa0c3c3b13a4574a27088319b2b1be5905
SapozhnikUA <48523324+SapozhnikUA@users.noreply.github.com>
2025-06-14 20:00:23 +0300

    Correction for new interface

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # waze_checker
+- Addons: 
+creates addition buttons Open All and Hide All in different reports of Checker Portal. 
+changes: background to the grey
 
-Скрипт создает дополнительную кнопку для одновременного открытия всех пересиленных на странице пермалинков. 
+v. 2025.06.14.001 - correction after changes of design of portal
--- checker-open-all-links.user.js
+++ checker-open-all-links.user.js
@@ -1,61 +1,80 @@
 // ==UserScript==
 // @name         WME AZ-Checker - UI Addons
 // @namespace    https://greasyfork.org/ru/users/160654-waze-ukraine
-// @version      2025.01.11.002
-// @description  Скрипт создает кнопки Open All и Hide All для всех пермалинков, расположенных на одной странице ошибок, в отдельных вкладках.
+// @version      2025.06.14.001
+// @description  Crates addition button Open All и Hide All for all permalinks which placed on one page of report
 // @author       Sapozhnik
-// @updateURL    https://greasyfork.org/ru/scripts/457575-wme-checker-open-all-links
 // @match        https://checker2.waze.uz/checker/errorlist/*
 // @match        https://checker.waze.uz/checker/errorlist/*
+// @downloadURL https://update.greasyfork.org/scripts/457575/WME%20AZ-Checker%20-%20UI%20Addons.user.js
+// @updateURL https://update.greasyfork.org/scripts/457575/WME%20AZ-Checker%20-%20UI%20Addons.meta.js
 // ==/UserScript==
 
 (function () {
     'use strict';
-    // Создаем Open All
-    // Создаём новый <span>
+
+    // Створюємо Open All кнопку
     var sp1 = document.createElement("span");
+    sp1.innerHTML = '<span><a href>Open All</a><br></span>';
+    sp1.onclick = function () { MyFunc() };
 
-    sp1.innerHTML = '<span><b><a href> Open All </a></b>&nbsp;&nbsp;</span>';
-    // Получаем ссылку на элемент, перед которым мы хот им вставить sp1
-    sp1.onclick = function () { MyFunc() }
+    // Знаходимо <thead>
+    var thead = document.querySelector('thead');
 
-    var sp2 = document.getElementsByClassName('page-links');
-    //Получаем ссылку на родителя sp2
-    var parentDiv = sp2[0].parentNode;
-    //    console.log (parentDiv);
+    if (thead) {
+        // Знаходимо перший рядок <tr> всередині <thead>
+        var firstTr = thead.querySelector('tr');
 
-    // Вставляем sp1 перед sp2
-    parentDiv.insertBefore(sp1, sp2[0]);
+        if (firstTr) {
+            // Знаходимо всі <th> в першому <tr>
+            var allThInFirstTr = firstTr.querySelectorAll('th');
 
-    // Создаем hide
-    // Создаём новый <span>
+            // Перевіряємо, чи існує другий <th> (індекс 1)
+            if (allThInFirstTr.length > 1) {
+                var secondTh = allThInFirstTr[1]; // Це другий <th>
+
+                // Вставляємо sp1 перед вмістом другого <th>
+                secondTh.insertBefore(sp1, secondTh.firstChild);
+                console.log("'Open All' button inserted into the second <th>.");
+            } else {
+                console.warn("Could not find a second <th> element in the first <tr> of <thead>.");
+            }
+        } else {
+            console.warn("Could not find the first <tr> element inside <thead>.");
+        }
+    } else {
+        console.warn("Could not find <thead> element on the page. 'Open All' button will not be added.");
+    }
+
+
+    // Створюємо Hide All кнопку (цей блок залишається майже без змін)
     var hide1 = document.createElement("th");
     hide1.innerHTML = '<a href> Hide All </a>';
 
-    // Получаем ссылку на элемент, перед которым мы хотим вставить hide1
     var hide2 = document.querySelectorAll('th');
-    // получаем кол-во элементов, чтоб найти последний
     var hide2_lenght = document.querySelectorAll('th').length - 1;
 
-    //Получаем ссылку на родителя hide2
-    var parentDiv_hide = hide2[hide2_lenght];
-    //           console.log (parentDiv_hide);
+    if (hide2.length > 0) {
+        var parentDiv_hide = hide2[hide2_lenght];
+        parentDiv_hide.innerHTML = hide1.innerHTML;
+        parentDiv_hide.onclick = function () { MyFunc_hide() };
+        console.log("'Hide All' button processed.");
+    } else {
+        console.warn("No <th> elements found for 'Hide All' button. It might not be added.");
+    }
 
-    // Вставляем hide1 вместо hide2
-    parentDiv_hide.innerHTML = hide1.innerHTML;
-    parentDiv_hide.onclick = function () { MyFunc_hide() }
-
-    // Смена дизайна
+    // Зміна дизайну
     document.querySelector('body').setAttribute("style","background-color: #f0f0f3");
 
 
     function MyFunc() {
-        // Array.from(document.getElementsByClassName('lnk')).map(i => { i.target = "_blank"; return i }).map(i => { i.click(); return i });
         Array.from(document.querySelectorAll('a[href$="/0/"][href*="/checker/go_waze"]')).map(i => { i.target = "_blank"; return i }).map(i => { i.click(); return i });
-        
     }
+
     function MyFunc_hide() {
         Array.from(document.querySelectorAll('a[href*="/hide_element"]')).map(i => { i.target = "_blank"; return i }).map(i => { i.click(); return i });
     }
+
+
 
 })();
